895c1cdc1a25f9fe1b7e1af14acf03e81b38d996
no.47 <jascal@163.com>
2018-10-27 14:41:25 +0800

    toolbar & custom textview
    module: app
    
    on branch: master

--- app/src/main/java/com/jascal/flora/mvp/view/MainActivity.java
+++ app/src/main/java/com/jascal/flora/mvp/view/MainActivity.java
@@ -1,9 +1,15 @@
 package com.jascal.flora.mvp.view;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.jascal.flora.R;
@@ -20,11 +26,36 @@ import java.util.List;
 public class MainActivity extends BaseActivity implements MainContract.view {
     private MainContract.presenter presenter;
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.navigation)
+    NavigationView navigationView;
+
     @BindView(R.id.recycler)
     RecyclerView recyclerView;
 
-//    @BindView(R.id.progress)
-//    ProgressBar progressBar;
+    @BindView(R.id.progress)
+    ProgressBar progressBar;
+
+    private void setNavItemListener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                return true;
+            }
+        });
+    }
+
+    private void setHeadViewClick() {
+        navigationView.getHeaderView(0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();//关闭导航菜单
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +73,11 @@ public class MainActivity extends BaseActivity implements MainContract.view {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setNavItemListener();
+        setHeadViewClick();
+
+        drawerLayout.openDrawer(navigationView);
     }
 
     @Override
@@ -51,7 +87,7 @@ public class MainActivity extends BaseActivity implements MainContract.view {
 
     @Override
     public void update(List<Feed> feeds) {
-//        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
         Toast.makeText(getApplicationContext(), "feeds num is " + feeds.size(), Toast.LENGTH_SHORT).show();
 
         FeedAdapter feedAdapter = new FeedAdapter(feeds);
@@ -62,4 +98,6 @@ public class MainActivity extends BaseActivity implements MainContract.view {
     public void error(String message) {
         Toast.makeText(getApplicationContext(), "get shots error:" + message, Toast.LENGTH_LONG).show();
     }
+
+
 }
---
+++ app/src/main/java/com/jascal/flora/widget/DrawableTextView.java
@@ -0,0 +1,95 @@
+package com.jascal.flora.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.jascal.flora.R;
+
+public class DrawableTextView extends android.support.v7.widget.AppCompatTextView {
+    public static final int LEFT = 1, TOP = 2, RIGHT = 3, BOTTOM = 4;
+
+    private int mHeight, mWidth;
+
+    private Drawable mDrawable;
+
+    private int mLocation;
+
+    public DrawableTextView(Context context) {
+        this(context, null);
+    }
+
+    public DrawableTextView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DrawableTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.DrawableTextView);
+
+        mWidth = a
+                .getDimensionPixelSize(R.styleable.DrawableTextView_drawable_width, 0);
+        mHeight = a.getDimensionPixelSize(R.styleable.DrawableTextView_drawable_height,
+                0);
+        mDrawable = a.getDrawable(R.styleable.DrawableTextView_drawable_src);
+        mLocation = a.getInt(R.styleable.DrawableTextView_drawable_location, LEFT);
+
+        a.recycle();
+        drawDrawable();
+    }
+
+    private void drawDrawable() {
+        if (mDrawable != null) {
+            Bitmap bitmap = ((BitmapDrawable) mDrawable).getBitmap();
+            Drawable drawable;
+            if (mWidth != 0 && mHeight != 0) {
+
+                drawable = new BitmapDrawable(getResources(), getBitmap(bitmap,
+                        mWidth, mHeight));
+
+            } else {
+                drawable = new BitmapDrawable(getResources(),
+                        Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(),
+                                bitmap.getHeight(), true));
+            }
+
+            switch (mLocation) {
+                case LEFT:
+                    this.setCompoundDrawablesWithIntrinsicBounds(drawable, null,
+                            null, null);
+                    break;
+                case TOP:
+                    this.setCompoundDrawablesWithIntrinsicBounds(null, drawable,
+                            null, null);
+                    break;
+                case RIGHT:
+                    this.setCompoundDrawablesWithIntrinsicBounds(null, null,
+                            drawable, null);
+                    break;
+                case BOTTOM:
+                    this.setCompoundDrawablesWithIntrinsicBounds(null, null, null,
+                            drawable);
+                    break;
+            }
+        }
+    }
+
+    public Bitmap getBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = (float) newWidth / width;
+        float scaleHeight = (float) newHeight / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        return Bitmap.createBitmap(bm, 0, 0, width, height, matrix, true);
+    }
+
+}
