f9b8cf944ef9cab75595461bf1218160f1c6a09b
no.47 <jascal@163.com>
2019-09-09 20:45:13 +0800

    set single process model
    module: app
    
    on branch: single_app_model

--- app/src/main/java/com/jascal/flora/mvp/photo/model/TensorModel.java
+++ app/src/main/java/com/jascal/flora/mvp/photo/model/TensorModel.java
@@ -20,18 +20,20 @@ import com.facebook.imagepipeline.request.ImageRequestBuilder;
 import com.jascal.flora.base.BaseModel;
 import com.jascal.tensor.IFactory;
 
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+
 public class TensorModel implements BaseModel {
-    //    private static final String MODEL_FILE = "models/stylize_quantized.pb";
-//    private static final String INPUT_NODE = "input";
-//    private static final String STYLE_NODE = "style_num";
+    private static final String MODEL_FILE = "models/stylize_quantized.pb";
+    private static final String INPUT_NODE = "input";
+    private static final String STYLE_NODE = "style_num";
     private static final int NUM_STYLES = 26;
-    //    private static final String OUTPUT_NODE = "transformer/expand/conv3/conv/Sigmoid";
+    private static final String OUTPUT_NODE = "transformer/expand/conv3/conv/Sigmoid";
     private Callback callback;
-    //    private TensorFlowInferenceInterface inferenceInterface;
+    private TensorFlowInferenceInterface inferenceInterface;
     private int model;
 
     public TensorModel(Context context) {
-//        inferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
+        inferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), MODEL_FILE);
     }
 
     public void setCallback(Callback callback) {
@@ -88,50 +90,44 @@ public class TensorModel implements BaseModel {
      * in ihave4cat`s mi4, 1024 would run into out of memory, sigh...
      * 512 is running ok, but it`s display worse.
      */
-//    private int desiredSize = 512;
-//    private final float[] styleVals = new float[NUM_STYLES];
-//    private int[] intValues = new int[desiredSize * desiredSize];
-//    private float[] floatValues = new float[desiredSize * desiredSize * 3];
+    private int desiredSize = 512;
+    private final float[] styleVals = new float[NUM_STYLES];
+    private int[] intValues = new int[desiredSize * desiredSize];
+    private float[] floatValues = new float[desiredSize * desiredSize * 3];
     private void stylizeImage(Bitmap bitmap) {
-//        bitmap = Bitmap.createScaledBitmap(bitmap, desiredSize, desiredSize, false);
-//        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-//
-//        for (int i = 0; i < intValues.length; ++i) {
-//            final int val = intValues[i];
-//            floatValues[i * 3] = ((val >> 16) & 0xFF) / 255.0f;
-//            floatValues[i * 3 + 1] = ((val >> 8) & 0xFF) / 255.0f;
-//            floatValues[i * 3 + 2] = (val & 0xFF) / 255.0f;
-//        }
-//        for (int i = 0; i < NUM_STYLES; ++i) {
-//            styleVals[i] = 0f;
-//        }
-//        styleVals[model] = 1f;
-//
-//        // Copy the input data into TensorFlow.
-//        Log.d("tensor", "Width: " + bitmap.getWidth() + ", Height: " + bitmap.getHeight());
-//        inferenceInterface.feed(
-//                INPUT_NODE, floatValues, 1, bitmap.getWidth(), bitmap.getHeight(), 3);
-//        inferenceInterface.feed(STYLE_NODE, styleVals, NUM_STYLES);
-//
-//        inferenceInterface.run(new String[]{OUTPUT_NODE}, false);
-//        inferenceInterface.fetch(OUTPUT_NODE, floatValues);
-//
-//        for (int i = 0; i < intValues.length; ++i) {
-//            intValues[i] =
-//                    0xFF000000
-//                            | (((int) (floatValues[i * 3] * 255)) << 16)
-//                            | (((int) (floatValues[i * 3 + 1] * 255)) << 8)
-//                            | ((int) (floatValues[i * 3 + 2] * 255));
-//        }
-//
-//        bitmap.setPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-//        Log.d("convert", "postResult bitmap");
-        try {
-            bitmap = factory.stylizeImage(bitmap, model);
-        } catch (RemoteException e) {
-            e.printStackTrace();
+        bitmap = Bitmap.createScaledBitmap(bitmap, desiredSize, desiredSize, false);
+        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        for (int i = 0; i < intValues.length; ++i) {
+            final int val = intValues[i];
+            floatValues[i * 3] = ((val >> 16) & 0xFF) / 255.0f;
+            floatValues[i * 3 + 1] = ((val >> 8) & 0xFF) / 255.0f;
+            floatValues[i * 3 + 2] = (val & 0xFF) / 255.0f;
         }
-        postResult(bitmap);
+        for (int i = 0; i < NUM_STYLES; ++i) {
+            styleVals[i] = 0f;
+        }
+        styleVals[model] = 1f;
+
+        // Copy the input data into TensorFlow.
+        Log.d("tensor", "Width: " + bitmap.getWidth() + ", Height: " + bitmap.getHeight());
+        inferenceInterface.feed(
+                INPUT_NODE, floatValues, 1, bitmap.getWidth(), bitmap.getHeight(), 3);
+        inferenceInterface.feed(STYLE_NODE, styleVals, NUM_STYLES);
+
+        inferenceInterface.run(new String[]{OUTPUT_NODE}, false);
+        inferenceInterface.fetch(OUTPUT_NODE, floatValues);
+
+        for (int i = 0; i < intValues.length; ++i) {
+            intValues[i] =
+                    0xFF000000
+                            | (((int) (floatValues[i * 3] * 255)) << 16)
+                            | (((int) (floatValues[i * 3 + 1] * 255)) << 8)
+                            | ((int) (floatValues[i * 3 + 2] * 255));
+        }
+
+        bitmap.setPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+        Log.d("convert", "postResult bitmap");
     }
 
     private void postResult(final Bitmap result) {
